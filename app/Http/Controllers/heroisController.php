d72a3e83c8fe52da605cf7ede7d0d8737e88e987
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2021-10-31 22:30:30 -0300

    Table / Model / Controller

---
+++ app/Http/Controllers/heroisController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\herois;
+use Illuminate\Http\Request;
+
+class heroisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\herois  $herois
+     * @return \Illuminate\Http\Response
+     */
+    public function show(herois $herois)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\herois  $herois
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(herois $herois)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\herois  $herois
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, herois $herois)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\herois  $herois
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(herois $herois)
+    {
+        //
+    }
+}
